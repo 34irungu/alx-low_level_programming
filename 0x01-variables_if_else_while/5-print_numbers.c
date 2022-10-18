4e580894401634367aa21232536e29b9d4eb788e
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-18 07:59:42 -0700

    added program that prints single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the integers 0-9
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		printf("%d", j);
+		j++;
+	}
+	printf("\n");
+	return (0);
+}
