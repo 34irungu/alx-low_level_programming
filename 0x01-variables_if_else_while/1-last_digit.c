b2b17e8f1f8f92ca812aaa43e3039dd3bc3e5946
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-18 05:45:51 -0700

    corrected code using betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 		}
 		else if (lastdig == 0)
 		{
-		  printf("Last digit of %d is %d and is 0\n", n, lastdig);
+			printf("Last digit of %d is %d and is 0\n", n, lastdig);
 		}
 		else if (lastdig != 0 && lastdig < 6)
 		{
