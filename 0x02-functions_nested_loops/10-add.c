ea2231b74b68d355b8c3ed16a4615f319886a4c7
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-19 10:26:19 -0700

    added function computes the addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - computes the addition of two integers
+ *
+ * @k: first integer
+ * @l: second integer
+ * Return: The result of the addition.
+ */
+int add(int k, int l)
+{
+	int sum = k + l;
+
+	return (sum);
+}
