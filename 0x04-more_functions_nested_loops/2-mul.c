4af55e25b378fb99d73c2e80d0d0239d74156d87
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-21 04:33:20 -0700

    added function multiplying integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - computes the multiplication of two integers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
