60582c4df7ddb8b6384a9a6943bf618f1261a51b
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-24 05:34:31 -0700

    added function prints diagnonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function draws a diagonal line
+ *
+ * @n: The number of times the line is printed
+ * Return: 0.
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
