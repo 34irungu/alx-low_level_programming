f02722b84a204d5c144cfc787e180570616fc5fd
Lameck Irungu <lameckirungu90@gmail.com>
2022-10-24 08:33:39 -0700

    added test file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Tests a function for 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+
+	positive_or_negative(i);
+	return (0);
+}
