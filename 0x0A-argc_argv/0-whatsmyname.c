ac6325936ba9ffaa1438dc1680ad98cfcc94eb8a
Lameck Irungu <lameckirungu90@gmail.com>
2022-11-12 17:41:17 -0800

    added a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+(#define UNUSED(argc) (void) (argc))
+/**
+ * main - prints the first argument passed to it
+ * @argc: The # of arguments passed to the function
+ * @argv: Pointer to strings whose meaning will be determined by the program
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project has C programs I used to learn about Arguments passed to the main function and how to compile with unused variables 
