05ec4ab576e58f4f3e1f216d3e6d51bffe51838d
Lameck Irungu <lameckirungu90@gmail.com>
2022-11-14 04:39:26 -0800

    fixed bug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,15 @@ char *create_array(unsigned int size, char c)
 	char *m;
 	unsigned int i;
 
-	m = malloc(sizeof(char) * size);
 
 	if (size == 0)
 		return (NULL);
+
+	m = malloc(sizeof(char) * size);
+
+	if (m == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		m[i] = c;
