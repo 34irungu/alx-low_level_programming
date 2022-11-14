fb065fbf2b541f7940c95e322a2d64c45f069c18
Lameck Irungu <lameckirungu90@gmail.com>
2022-11-14 04:30:03 -0800

    added readme header file and function that creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters
+ * @size: size of the memory to print
+ * @c: Initializes the array
+ *
+ * Return: Null if size = 0, of a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *m;
+	unsigned int i;
+
+	m = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		m[i] = c;
+	}
+	m[i] = '\0';
+	return (m);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+This project is about:
+1. automatic and dynamic allocation
+2. malloc and free and how to use them
+3. why and when to use malloc 
+4. How to use valgrid to check for memory leak 
