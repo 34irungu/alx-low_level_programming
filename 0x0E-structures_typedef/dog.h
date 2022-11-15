c5e2f3ed0db9d400f61772d14a9217b309a6b3c7
Lameck Irungu <lameckirungu90@gmail.com>
2022-11-15 08:01:44 -0800

    defined a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: the age of the dog
+ *
+ * Description: The attributes of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} my_dog;
+#endif
