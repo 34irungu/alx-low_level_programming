fdff8a44c8822760ba5e8eb9995fed324f335606
Lameck Irungu <lameckirungu90@gmail.com>
2022-11-25 04:17:28 -0800

    adds program prints combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
