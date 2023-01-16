d6f43648f88f572f455ca18378b2581d5950b97e
Lameck Irungu <lameckirungu90@gmail.com>
2023-01-16 01:11:30 -0800

    added program that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: An integer
+ */
+int _atoi(char *s)
+{
+	int n = 0, i = 0, sign = 1, rep = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			rep = 1;
+			n = (n * 10) + (s[i] - '0');
+			i++;
+		}
+
+		if (rep == 1)
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	n = n * sign;
+	return (n);
+}
