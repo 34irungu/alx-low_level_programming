562b76a8566774d1f89d36bb3defde52bb80b7ad
Lameck Irungu <lameckirungu90@gmail.com>
2023-01-17 01:59:18 -0800

    adds a prime number checker function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is a  prime number
+ * @n: The number to check.
+ *
+ * Return: integer value
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
