6aaae47f998459cc74926db239a601e2861b5d60
Lameck Irungu <lameckirungu90@gmail.com>
2023-01-20 05:31:14 -0800

    added a program that frees all the previous memory allocations of struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * free_dog - ...
+  * @d: The structure to be freed
+  *
+  * Return: Nothing.
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
