7f81d53b42e5f2044b72a2a9538445ba94ffa9ee
Lameck Irungu <lameckirungu90@gmail.com>
2023-02-10 05:23:05 -0800

    adds modified header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,12 @@
 #ifndef _HEADER_
 #define _HEADER_
+
 #include <stddef.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
